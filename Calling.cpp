2a3a2891955df6fbe295d6acf33a2ae24abd9f30
Doyoon Kim <cheeezing_u@naver.com>
2018-04-30 22:34:52 +0900

    main loop modifying

--- Calling.cpp
+++
@@ -1,9 +0,0 @@
-#include "global.h"
-
-Calling::Calling(char* buffer, int msgLen) {
-	strcpy(this->buffer, buffer);
-	this->msgLen = msgLen;
-} 
-char* Calling::getCallingNumber() {
-	return &this->buffer[2];
-}
